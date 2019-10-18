679b0bf62290dd3cf24b606e4abe0b4eecde9356
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2019-10-18 13:21:44 +0200

    match by wildcards

--- src/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,8 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(public void add*())")
+//    @Before("execution(public void add*())")
+    @Before("execution(* add*())")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
--- src/com/spring/aopdemo/dao/MembershipDAO.java
+++ src/com/spring/aopdemo/dao/MembershipDAO.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class MembershipDAO {
 
-    public void addSillyMember() {
+    public boolean addSillyMember() {
         System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+        return true;
     }
 }
